6b51aae5cc256074db5c7fa75cf286ad2ba26707
Roberto Spotti <roberto.spotti@gmail.com>
2019-05-27 23:01:55 +0200

    Versione iniziale

---
+++ src/Outer.java
@@ -0,0 +1,62 @@
+
+
+public class Outer {
+	private String aaa;
+	public class Inner {
+		private int ugo;
+		void setUgo(int u) {
+			ugo=u;
+		}
+		int getUgo() {
+			System.out.println(aaa);
+			return ugo;
+		}
+	}
+    static class Internal {
+    	public int uno;
+
+		public int getUno() {
+			return uno;
+		}
+
+		public void setUno(int uno) {
+			this.uno = uno;
+		}
+    	
+    }
+	public Outer() {
+		aaa="Cicocico";	
+		Inner in=new Inner();
+		in.setUgo(7);
+		System.out.println("Sono dentro="+in.getUgo());
+	}
+	
+	public void innesto() {
+		int valore=0;
+		class Interna {
+			private int gedeone=13;
+			public void printGedeone() {
+				System.out.println("Gedeone="+gedeone);
+				gedeone=valore;
+				// valore++; //--> Non possibile
+			}
+		}
+		
+		Interna inner=new Interna();
+		inner.printGedeone();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Outer ou=new Outer();
+		Outer.Inner in=ou.new Inner();
+		in.setUgo(12);
+		System.out.println(""+in.getUgo());
+		ou.innesto();
+		
+		Internal instestinal=new Internal(); // Se dichiarata statica diventa top class!!
+		
+	}
+
+}
